e5a988cfac718afec35d48d09dcd65eae0620d4b
Alex Curtis <hello@devmarketer.io>
2017-07-23 23:58:26 +0600

    Initial commit for my first laravel project

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravel Quickstart - Basic</title>
+	<!-- CSS And Javascript -->
+</head>
+	<body>
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<!-- Navbar Contents -->
+			</nav>
+		</div>
+
+		@yield('content')
+	</body>
+</html>
